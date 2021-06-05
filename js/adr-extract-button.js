8b0f999227b36bb6d6ecaef7cdb9a6e90d25fc16
Marine Dunstetter <bluecut.official@gmail.com>
2021-06-05 10:31:09 +0200

    Build style.css with saas (#3)
    * feat: build style.css with saas
    
    * refactor: clean style with sass
    
    * refactor: move styles in a folder

--- js/adr-extract-button.js
+++ js/adr-extract-button.js
@@ -14,7 +14,7 @@ class AdrExtractButton extends HTMLElement {
     var shadow = this.attachShadow({mode: 'closed'});
     shadow.innerHTML = `
       <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
-      <link rel="stylesheet" type="text/css" href="buttons.css">
+      <link rel="stylesheet" type="text/css" href="styles/style.css">
     `;
     shadow.appendChild(wrapper);  
   }
