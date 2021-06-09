beb8d5d943ad2a0940b2744ed685689de7094dcc
Marine Dunstetter <bluecut.official@gmail.com>
2021-06-09 21:09:29 +0200

    refactor: use modules

--- js/ui.js
+++ js/ui.js
@@ -1,130 +1,5 @@
-let selectors = {}
-
-/**
- * Get element by id
- * Elements that were already requested are registered in "selectors" array for performance
- */
-function select(id) {
-  let selector = selectors[id]
-  if (!selector) {
-    selector = document.getElementById(id)
-    selectors[id] = selector
-  }
-  return selector
-}
-
-/**
- * Add or remove the class "toggleClass" on all given selectors
- */
-function toggleClass(selectors, toggleClass, isAdd) {
-  selectors.forEach((id) => {
-    if (isAdd) {
-      select(id).classList.add(toggleClass)
-    } else {
-      select(id).classList.remove(toggleClass)
-    }
-  })
-}
-
-/**
- * Switch the menu from burger icon to full display
- */
-// eslint-disable-next-line no-unused-vars
-function toggleMenu() {
-  let active = 'active'
-  select('layout').classList.toggle(active)
-  select('menu').classList.toggle(active)
-  select('menuLink').classList.toggle(active)
-}
-
-/**
- * Hide the whole menu to show only the burger icon
- */
-function deactivateMenu() {
-  toggleClass(['layout', 'menu', 'menuLink'], 'active', false)
-}
-
-function showMenuInfo() {
-  select('booksLink').classList.remove('pure-menu-selected')
-  select('booksLink').children[0].removeAttribute('aria-current')
-  select('infoLink').classList.add('pure-menu-selected')
-  select('infoLink').children[0].setAttribute('aria-current', 'page')
-  toggleClass(['books', 'sorting-menu'], 'hidden', true)
-  toggleClass(['info'], 'hidden', false)
-  deactivateMenu()
-}
-
-function showMenuBooks() {
-  select('infoLink').classList.remove('pure-menu-selected')
-  select('infoLink').children[0].removeAttribute('aria-current')
-  select('booksLink').classList.add('pure-menu-selected')
-  select('booksLink').children[0].setAttribute('aria-current', 'page')
-  toggleClass(['books', 'sorting-menu'], 'hidden', false)
-  toggleClass(['info'], 'hidden', true)
-  deactivateMenu()
-}
-
-/**
- * Functions related to books sorting
- */
-
-function sortBy(attribute) {
-  [...select('books').children]
-    .sort((a,b) => a.getAttribute(attribute) > b.getAttribute(attribute) ? 1 : -1)
-    .forEach(node => select('books').appendChild(node))
-}
-
-function sortByChrono() {
-  sortBy('data-chrono')
-  select('sorting').textContent = 'Ordre chronologique'
-  select('sorting').setAttribute('href', '#livres?sorting=chrono')
-  toggleClass(['sorting-date', 'sorting-latest'], 'pure-menu-selected', false)
-  toggleClass(['sorting-chrono'], 'pure-menu-selected', true)
-}
-
-function sortByDate() {
-  sortBy('data-date')
-  select('sorting').textContent = 'Date de publication'
-  select('sorting').setAttribute('href', '#livres?sorting=date')
-  toggleClass(['sorting-chrono', 'sorting-latest'], 'pure-menu-selected', false)
-  toggleClass(['sorting-date'], 'pure-menu-selected', true)
-}
-
-function sortByLatest() {
-  sortBy('data-latest')
-  select('sorting').textContent = 'Plus récent'
-  select('sorting').setAttribute('href', '#livres?sorting=latest')
-  toggleClass(['sorting-chrono', 'sorting-date'], 'pure-menu-selected', false)
-  toggleClass(['sorting-latest'], 'pure-menu-selected', true)
-}
-
-/**
- * Set up the page depending on the current URL
- */
-function setUpPage() {
-  let hash = window.location.hash
-  if (hash.includes('#info')) {
-    showMenuInfo()
-  } else if (hash.includes('#livres')) {
-    showMenuBooks()
-    if (hash.includes('?')) {
-      let params = hash.split('?').reverse()[0]
-      switch (params) {
-      default:
-        break
-      case 'sorting=date':
-        sortByDate()
-        break
-      case 'sorting=chrono':
-        sortByChrono()
-        break
-      case 'sorting=latest':
-        sortByLatest()
-        break
-      }
-    }
-  }
-}
+import { setSorting, setUpPage } from './utils/setup-page'
+import { deactivateMenu, toggleMenu } from './utils/sidebar'
 
 /**
  * Detection of a URL change reset the page
@@ -133,20 +8,16 @@ window.onhashchange = function() {
   setUpPage()
 }
 
-/*
+/**
  * setUpPage called when DOM is loaded
  */
 window.onload = function() {
   setUpPage()
 }
 
-/*
- * Functions defined globaly as they are called in the HTML on click
+/**
+ * Methods bound to DOM elements need to be assigned globally 
  */
-window.toggleMenu = toggleMenu
+window.setSorting = setSorting
 window.deactivateMenu = deactivateMenu
-window.sortByChrono = sortByChrono
-window.sortByDate = sortByDate
-window.sortByLatest = sortByLatest
-window.showMenuInfo = showMenuInfo
-window.showMenuBooks = showMenuBooks
+window.toggleMenu = toggleMenu
---
+++ js/utils/generics.js
@@ -0,0 +1,33 @@
+/**
+ * Get element by id
+ * Elements that were already requested are registered in "selectors" array for performance
+ */
+export function select(id) {
+  let selector = window.selectors[id]
+  if (!selector) {
+    selector = document.getElementById(id)
+    window.selectors.id = selector
+  }
+  return selector
+}
+
+/**
+ * Add or remove the class "toggleClass" on all given selectors
+ */
+export function toggleClass(selectors, toggleClass, isAdd) {
+  selectors.forEach((id) => {
+    if (isAdd) {
+      select(id).classList.add(toggleClass)
+    } else {
+      select(id).classList.remove(toggleClass)
+    }
+  })
+}
+
+/**
+ * Return the part of the URL corresponding to location hash according to current values,
+ * designed to work only when called on books page
+ */
+export function getURL() {
+  return `#${window.baseURL}?${window.sortingParam}`
+}
