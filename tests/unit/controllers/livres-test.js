13a78f13152b19165d761584e0f831462e972a4d
Marine Dunstetter <bluecut.official@gmail.com>
2024-05-31 19:37:50 +0200

    use splitAtRoute option for livres and que-choisir, as they use a lot of specific components (#58)

--- tests/unit/controllers/livres-test.js
+++ tests/unit/controllers/livres-test.js
@@ -1,10 +1,14 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'site-books-ember/tests/helpers';
+import LivresRoute from 'site-books-ember/controllers/livres';
 
 module('Unit | Controller | livres', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
+  hooks.beforeEach(function () {
+    this.owner.register('controller:livres', LivresRoute);
+  });
+
   test('it exists', function (assert) {
     let controller = this.owner.lookup('controller:livres');
     assert.ok(controller);
